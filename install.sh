b930098a1d36afe9e5c103205934b98132413270
Hank <Hank@Hanks-MacBook-Pro.local>
2012-12-01 20:33:23 +0100

    Prepared the install script

--- install.sh
+++ install.sh
@@ -1,8 +1,10 @@
-for item in ~/.{vimrc.after,vimrc.before,tmux.conf,extra,non_existent_file,janus}; do
+for item in {vimrc.after,vimrc.before,tmux.conf,extra,non_existent_file,janus}; do
   if [ -r "$item" ]; then
     echo "There's already a file/directory called $item in $HOME"
   else
-    echo "i would like to copy $item to $HOME"
+    echo "Linking $item to $HOME"
+    echo "Going to do this: ln -s $PWD/$item $HOME/$item" 
+    # ln -s "$PWD/$item" "$HOME/$item"
   fi
 done
 unset item 
